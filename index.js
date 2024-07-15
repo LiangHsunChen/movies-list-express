bcd83e85be43130eedd6a7f50934a414a64470f1
LiangHsunChen <lianghsunchen@gmail.com>
2024-07-15 16:27:16 +1000

    Added GET with id endpoint to get a specific movie with the given id

--- index.js
+++ index.js
@@ -5,11 +5,25 @@ const PORT = 3000;
 app.use(express.json());
 
 let movies = {};
+let id_counter = 1;
 
 // GET /movies
 app.get("/movies", (req, res) => {
   console.log("GET /movies");
   res.json(movies);
+});
+
+// GET /movies/:id
+app.get("/movies/:id", (req, res) => {
+  const id = req.params.id;
+  const movie = movies[id];
+  if (movie) {
+    res.json(movie);
+    console.log("GET /movies/:id", id);
+  } else {
+    res.status(404).json({ message: "Movie not found" });
+    console.log("Movie not found");
+  }
 });
 
 // POST /movies
