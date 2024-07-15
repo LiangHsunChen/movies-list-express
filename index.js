c771879e260b8b6be14b9de4f973d099abbdc961
LiangHsunChen <lianghsunchen@gmail.com>
2024-07-15 16:25:54 +1000

    Added POST endpoint to add new movie

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ app.get("/movies", (req, res) => {
   res.json(movies);
 });
 
+// POST /movies
+app.post("/movies", (req, res) => {
+  const movie = req.body;
+  movie.id = id_counter++;
+  movies[movie.id] = movie;
+
+  res.status(201).json(movie);
+
+  console.log("POST /movies", movie);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
