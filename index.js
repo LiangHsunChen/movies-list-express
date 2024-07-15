785940138d7281bb7ee2475edec438dd75a2d04b
LiangHsunChen <lianghsunchen@gmail.com>
2024-07-15 16:24:40 +1000

    Added GET endpoint to get all movies

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ app.use(express.json());
 
 let movies = {};
 
+// GET /movies
+app.get("/movies", (req, res) => {
+  console.log("GET /movies");
+  res.json(movies);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
